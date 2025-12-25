dbd18808994308de9f81d2057f72823546706784
kunu <soheil.pirani8484@gmail.com>
2025-12-25 15:13:53 +0330

    added turn implementation, pin count limitation

--- AI_game.py
+++ AI_game.py
@@ -11,9 +11,9 @@ PIN_SIZE = (60, 60)
 PIN_MARGIN = 20
 
 PIN_TYPES = [
-    {"value": 1, "color": "red"},
-    {"value": 2, "color": "green"},
-    {"value": 3, "color": "blue"},
+    {"value": 3, "color": "red", "health": 3},
+    {"value": 2, "color": "green", "health": 2},
+    {"value": 1, "color": "blue", "health": 1},
 ]
 
 
@@ -50,10 +50,24 @@ def get_grid_offset(screen_width, screen_height):
 
     return offset_x, offset_y
 
+def end_turn():
+    global selected_pin,turn,pin_counts_p1,pin_counts_p2,pin_counts
+
+    for pin in pin_selectors:
+        if selected_pin is pin:
+            pin.selected = False
+    selected_pin = None
+    turn = turn ^ 1
+    if turn == 0:
+        pin_counts = pin_counts_p1
+    else:
+        pin_counts = pin_counts_p2
+
 class PinSelector:
-    def __init__(self, x, y, size, value, color):
+    def __init__(self, x, y, size, value, color,health):
         self.value = value
         self.color = color
+        self.health = health
         self.surface = pygame.Surface(size)
         self.surface.fill(color)
         self.rect = self.surface.get_rect(topleft=(x, y))
@@ -79,11 +93,25 @@ for i, pin in enumerate(PIN_TYPES):
             PIN_SIZE,
             pin["value"],
             pin["color"],
+            pin["health"],
         )
     )
 
+#player turns
+turn = 1  # 0 is player, 1 is AI
+turn_counter = 0
+#pins
 selected_pin = None
 moving_pin = None
+
+pin_counts = [0,0,0,0]
+pin_counts_p1 = [0,3,3,2]
+pin_counts_p2 = [0,3,3,2]
+
+# pin_rock = 3
+# pin_lily = 3
+# pin_lotus = 2
+end_turn()
 running = True
 while True:
     for event in pygame.event.get():
@@ -94,9 +122,10 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             #left click
             mouse_pos = event.pos
+            #selecting the pin to place
             for pin in pin_selectors:
-                if pin.is_clicked(mouse_pos) and selected_pin is not pin:
-                    print("selecting pin")
+                if pin.is_clicked(mouse_pos) and selected_pin is not pin and pin_counts[pin.value] != 0:
+                    print("selecting pin:" , pin.color, " ", pin.health)
                     selected_pin = pin
                     for p in pin_selectors:
                         p.selected = False
@@ -106,17 +135,28 @@ while True:
                     selected_pin = None
                     pin.selected = False
                     break
+            #clicking the board
             else:
+                #placing a pin
                 if selected_pin and not moving_pin:
                     for cell in cells:
                         if cell.rect.collidepoint(mouse_pos) and cell.value == 0:
-                            cell.set_pin(selected_pin.value, selected_pin.color)
+                            if turn == 0 and cell.row >= GRID_ROWS - 2:
+                                cell.set_pin(selected_pin.value, selected_pin.color)
+                                pin_counts_p1[selected_pin.value] -= 1
+                                print(selected_pin.color, pin_counts_p1[selected_pin.value])
+                                end_turn()
+                            elif turn == 1 and cell.row <= 1 :
+                                cell.set_pin(selected_pin.value, selected_pin.color)
+                                pin_counts_p2[selected_pin.value] -= 1
+                                print(selected_pin.color, pin_counts_p2[selected_pin.value])
+                                end_turn()
                             break
+                #moving a pin
                 else:
                     for cell in cells:
                         if cell.rect.collidepoint(mouse_pos) and cell.value != 0:
                             moving_pin = (cell.value, cell.color,cell.row, cell.col)
-                            print("moving_pin", moving_pin)
                             break
                         elif cell.rect.collidepoint(mouse_pos) and cell.value == 0 and moving_pin is not None:
                             cell.set_pin(moving_pin[0], moving_pin[1])
@@ -124,6 +164,7 @@ while True:
                                 if cell.row == moving_pin[2] and cell.col == moving_pin[3]:
                                     cell.set_pin(0, "white")
                             moving_pin = None
+                            end_turn()
 
 
 
